85c6664f2d2dbe52dff73e1d9aed186e7d2bc495
bsnugroho7 <44954738+bsnugroho7@users.noreply.github.com>
2025-07-31 23:37:47 +0700

    Update_Jul_2025

---
+++ menu.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("menu-toggle");
+    const nav = document.getElementById("nav");
+  
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  });
+  
